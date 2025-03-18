5a2aecd19ce6872a799f9661bdc5dba0d44105d6
Shanny254 <sharlynenyaboke@gmail.com>
2025-03-18 13:46:59 +0300

    assignment3

---
+++ ASSIGNMENTS/Assignment3.py
@@ -0,0 +1,17 @@
+# Step 1: Create an empty list
+my_list = []
+
+# Step 2: Append elements 10, 20, 30, 40
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Step 3: Insert 15 at the second position (index 1)
+my_list.insert(1, 15)
+
+# Step 4: Extend my_list with another list [50, 60, 70]
+my_list.extend([50, 60, 70])
+
+# Step 5: Remove the last element
+my_list.pop()
