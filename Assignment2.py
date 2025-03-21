81c69255e2726085ac371f639fd00ead49b175c6
Shanny254 <157687126+Shanny254@users.noreply.github.com>
2025-03-21 18:30:21 +0300

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -9,15 +9,19 @@ my_list.append(40)
 
 # Step 3: Insert 15 at the second position (index 1)
 my_list.insert(1, 15)
+print(my_list)
 
 # Step 4: Extend my_list with another list [50, 60, 70]
 my_list.extend([50, 60, 70])
+print(my_list)
 
 # Step 5: Remove the last element
 my_list.pop()
+print(my_list)
 
 # Step 6: Sort the list in ascending order
 my_list.sort()
+print(my_list)
 
 # Step 7: Find and print the index of value 30
 index_of_30 = my_list.index(30)
